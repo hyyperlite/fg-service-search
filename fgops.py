6797460fd4622466b36ca8890307c23ae493fac0
Nick Petersen <hyyperlite@gmail.com>
2017-12-11 17:14:08 -0500

    Code cleanup

--- fgops.py
+++ fgops.py
@@ -1,8 +1,8 @@
 from ftntlib import FortiOSREST
 import json
 import argparse
-import ssl
-import sys
+#import ssl
+#import sys
 import base64
 import os
 
@@ -92,8 +92,6 @@ svcsbyname = []    # List to contain named services to search for
 # Create the list of fortigates from file, or if no file, from cli arguments
 if args.fglist:
     for line in fglist:
-        # fg,login,passwd = line.split()
-        # fortigates[fg] = {'login': login, 'passwd': passwd}
         fg,vdom,login,passwd = line.split()
         fortigates[fg + '-' + vdom] = {'host': fg, 'vdom': vdom, 'login': login, 'passwd': passwd}
     fglist.close()
@@ -118,41 +116,40 @@ if args.servicelist:
         svcsbyname.append(line.rstrip())
     servicelist.close()
 
-### Instantiate needed vars, lists and dicts
+# Instantiate needed vars, lists and dicts
 serviceMatch = {}
 policyMatch = {}
-serviceCheck = True
-policyServiceCheck = True
-#verbose = True
+serviceCheck = False
+policyCheck = True
+vipCheck = True
+# verbose = True
 allowedprotos = [6, 17, 132]
+allowedprotosname = ['tcp', 'udp', 'sctp']
 
 
-### Function to identify if a given service object will match for a specified protocol/port
-def findService(service, searchproto, searchport):
+# Function to identify if a given service object will match for a specified protocol/port
+def find_service(service, searchproto, searchport):
     prange = searchproto + '-portrange'
     lowport = []
     highport = []
-
 
     # Check if object is associated to protocol being searched for
     if service[prange] == '':
         return False
 
-    ### portrange fields in FOS may report in various formats. Differs depending on if just lowport was
-    ### specified or low/high ports and also based on if source ports specified or multiple low/high,src/dst
+    # Portrange fields in FOS may report in various formats. Differs depending on if just lowport was
+    # specified or low/high ports and also based on if source ports specified or multiple low/high,src/dst
     # ':' denotes delimiter between dport/dport-range and sport/sport-range
     if ':' in service[prange]:
         dports, sports = service[prange].split(':')
     else:
         dports = service[prange]
-        sports = ''
 
-    # ' ' deliniates multiple dport/dport-ranges
+    # ' ' delineates multiple dport/dport-ranges
     if ' ' in dports:
         dports = dports.split()
     else:
         dports = [dports]
-
 
     # '-' deliniates lowport and highport if a range is given
     for y in range(len(dports)):
@@ -164,31 +161,70 @@ def findService(service, searchproto, searchport):
             lowport = dports
             highport = dports
 
-
     # Created a list of low ports and high ports for this object, now analyze each to identify if
     # the protocol/port we are searching for falls in any of the ranges provided.  (note, we do not yet
     # consider source ports)
     for idx in range(len(lowport)):
-        # print(str(searchproto) + "/" + str(searchport) + " Search For: " + service['name'] + "\t" + service[
-        #     prange] + "\t" + str(service['protocol-number']))
         if int(service['protocol-number']) not in allowedprotos:
             return False
 
         if (int(lowport[idx]) <= int(searchport)) and (int(searchport) <= int(highport[idx])):
-            # print('test')
-            # print(str(searchproto) + "/" + str(searchport) + " MATCH " + service['name'] + "\t" + service[prange] + "\t" + str(service['protocol-number']))
             return service['name']
         else:
             return False
 
-### Function to find match from list of services provided to services contained in policy
-def findPolicyService(policies, service):
+
+# Function to identify if a given vip object will match for a specified protocol/port
+def find_vip(vip, searchproto, searchport):
+
+    # Check if portforwarding is enabled for this vip
+    if vip['portforward'] == 'disable':
+        return False
+
+    # Check if vip proto is one of supported by this script
+    if vip['protocol'] not in allowedprotosname:
+        return False
+
+    # Make sure protocol matches search protocol
+    if searchproto != vip['protocol']:
+        return False
+
+    # Find port range lower/upper
+    dports = vip['mappedport']
+
+    # '-' delineates lowport and highport if a range is given
+    if '-' in dports:
+        low, high = dports.split('-')
+        lowport = low
+        highport = high
+    else:
+        lowport = dports
+        highport = dports
+
+    # Created a list of low ports and high ports for this object, now analyze each to identify if
+    # the protocol/port we are searching for falls in any of the ranges provided.  (note, we do not yet
+    # consider source ports)
+    if (int(lowport) <= int(searchport)) and (int(searchport) <= int(highport)):
+        return vip['name']
+    else:
+        return False
+
+
+# Function to find match from list of services provided to services contained in policy
+def find_policy_match(policies, searchitem, objtype):
     policymatch = []
 
     for policy in policies:
-        for fgservice in policy['service']:
-            if service == fgservice['name']:
-                policymatch.append(policy['policyid'])
+        if objtype == 'fgservice' or objtype == 'fgservicegrp':
+            for fgobject in policy['service']:
+                if searchitem == fgobject['name']:
+                    policymatch.append(policy['policyid'])
+        elif objtype == 'fgvip' or objtype == 'fgvipgrp':
+            for fgobject in policy['dstaddr']:
+                if searchitem == fgobject['name']:
+                    policymatch.append(policy['policyid'])
+        else:
+            continue
 
     if len(policymatch) > 0:
         return policymatch
@@ -199,11 +235,14 @@ def findPolicyService(policies, service):
 try:
     for fg in fortigates:
         serviceMatch = {}  # Dict that we'll store all match info in
-        print('fg: ' + str(fg))
+        # print('fg: ' + str(fg))
         fgt = FortiOSREST()
-        #if verbose: fgt.debug('on')
+        # if verbose: fgt.debug('on')
         result = fgt.login(fortigates[fg]['host'], fortigates[fg]['login'], fortigates[fg]['passwd'])
 
+        ############################################
+        # Service Matching
+        ###########################################
         if serviceCheck:
             # Get list of all custom services defined on fortigate
             response = fgt.get('cmdb', 'firewall.service', 'custom', parameters={'vdom': fortigates[fg]['vdom']})
@@ -216,16 +255,20 @@ try:
                     lport = portproto[item]['port']
                     lprotoport = lproto + "/" + str(lport)
 
-                    result = findService(fgsvc, lproto, lport)
+                    result = find_service(fgsvc, lproto, lport)
                     if result:
-                        if not lprotoport in serviceMatch.keys():
+                        if lprotoport not in serviceMatch.keys():
                             serviceMatch[lprotoport] = []
 
-                        serviceMatch[lprotoport].append({result : {'type': 'fgservice', 'policymatch': [], 'groups': []}})
+                        serviceMatch[lprotoport].append({result: {'type': 'fgservice', 'policymatch': [],
+                                                                  'groups': []}})
 
-            ## Also need to check if any of the services that were matched are in an address group
-            ## If so then we'll store those address groups, and later check to see if those are used
-            ## in a policy
+            #######################################
+            # Service Group Matching
+            #######################################
+            # Also need to check if any of the services that were matched are in an address group
+            # If so then we'll store those address groups, and later check to see if those are used
+            # in a policy
             response = fgt.get('cmdb', 'firewall.service', 'group', parameters={'vdom': fortigates[fg]['vdom']})
             json_response = json.loads(response)
 
@@ -236,42 +279,58 @@ try:
                             for svc_key, svc_val in service_val.items():
                                 if svc_val['type'] == 'fgservice':
                                     if svc_key == member['name']:
-                                        #print('match: ' + member['name'] + ' in svc group: ' + str(group['name']))
 
                                         # add new fgservice group entry to serviceMatch
-                                        serviceMatch[protoport].append({group['name'] : {'type': 'fgservicegrp'}})
+                                        serviceMatch[protoport].append({group['name']: {'type': 'fgservicegrp',
+                                                                                        'policymatch': []}})
 
                                         # update existing service object with related servicegroup map
                                         serviceMatch[protoport][service_key][svc_key]['groups'].append(group['name'])
 
-        #print(serviceMatch)
-        print(json.dumps(serviceMatch, indent=2, sort_keys=True))
+        ##########################################
+        # VIP Matching
+        ##########################################
+        if vipCheck:
+            # Get list of all vips defined on fortigate
+            response = fgt.get('cmdb', 'firewall', 'vip', parameters={'vdom': fortigates[fg]['vdom']})
+            json_response = json.loads(response)
 
+            # For each vip object returned from FG, search for port match
+            for fgvip in json_response['results']:
+                for item in portproto:
+                    proto = portproto[item]['protocol']
+                    port = portproto[item]['port']
+                    protoport = proto + "/" + str(port)
 
+                    result = find_vip(fgvip, proto, port)
 
+                    if result:
+                        if protoport not in serviceMatch.keys():
+                            serviceMatch[protoport] = []
 
+                        serviceMatch[protoport].append({result: {'type': 'fgvip', 'policymatch': [], 'groups': []}})
+
+        ##########################################
+        # Policy Matching
+        ##########################################
         # Given a list of service objects, check to see if any policies use the service objects
         # If there is a match, we will add the policyid of the matching rule to the services object data
-        if policyServiceCheck:
+        if policyCheck:
             # Query the FG for defined policies
             response = fgt.get('cmdb', 'firewall', 'policy', parameters={'vdom': fortigates[fg]['vdom']})
             json_response = json.loads(response)
-            # print(json.dumps(json_response['results'], indent=2, sort_keys=True))
 
             for protoport in serviceMatch:
                 for service_key, service_value in enumerate(serviceMatch[protoport]):
                     for svc_key, svc_value in service_value.items():
-                        if svc_value['type'] == 'fgservice':
-                            result = findPolicyService(json_response['results'], svc_key)
-                            if result:
-                                serviceMatch[protoport][service_key][svc_key]['policymatch'].append(result)
+                        result = find_policy_match(json_response['results'], svc_key, svc_value['type'])
+                        if result:
+                            serviceMatch[protoport][service_key][svc_key]['policymatch'].append(result)
 
-
-            # print('*****************************')
-            # print('*****************************')
-            # print(json.dumps(serviceMatch, indent=2, sort_keys=True))
-            #print(serviceMatch)
-
+        serviceMatch['fortigate'] = fortigates[fg]['host']
+        serviceMatch['vdom'] = fortigates[fg]['vdom']
+        print(json.dumps(serviceMatch, indent=2, sort_keys=True))
+        # print(serviceMatch)
 
         fgt.logout()
 # on exception, try to log out
