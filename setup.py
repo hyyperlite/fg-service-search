8ddc91f50895d8242cfe0daa4fcaad1369399acf
Nick Petersen <hyyperlite@gmail.com>
2017-12-12 14:57:26 -0500

    added install of ftntlib to setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='FG Service Search',
+    version='0.1',
+    install_requires=['datetime', 'simplejson', 'argparse', 'lxml'],
+    url='',
+    license='',
+    author='Nick Petersen',
+    author_email='',
+    description='Search FortiGate(s) config searching for objects matching one or more supplied protocol/port '
+                'combinations. Searches services, servicegroups, vips, vipgroups.  Then see if the identified objects'
+                'used in policies, returning list of objects and policy usage'
+)
+
+import os
+print("installing ftnt lib from source")
+os.system("cd ./dependencies/ftntlib-0.4.0.dev13; python3 ./setup.py install")
